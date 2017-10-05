59691939e53f9f0a31b28f500ec7d946e29f884d
Amanda Rawls <jarfk6@mail.umsl.edu>
2017-10-05 07:41:28 -0500

    removed packaging

--- Adversarial.java
+++ Adversarial.java
@@ -1,5 +1,3 @@
-package p3;
-
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
--- Minimax.java
+++ Minimax.java
@@ -1,5 +1,3 @@
-package p3;
-
 import java.util.Calendar;
 import java.util.List;
 
